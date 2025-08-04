043bc999258ac430504497e123187a8daba061a3
Ilya Pridanov <ik.front2558988@gmail.com>
2025-08-04 21:46:00 +0300

    feat:font

--- source/js/script.ts
+++ source/js/script.ts
@@ -1,11 +1,9 @@
-import getHeaderMobile from './modules/getHeaderMobile';
 import getSlider from './modules/getSlider';
 import sortPopupToggle from './modules/sortPopupToggle';
 import filterPopupToggle from './modules/filterPopupToggle';
 import basketPopupToggle from './modules/basketPopupToggle';
 import getBasket from './modules/getBasket';
 
-getHeaderMobile();
 getSlider();
 sortPopupToggle();
 filterPopupToggle();
