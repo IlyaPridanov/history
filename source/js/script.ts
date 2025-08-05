6789beeebb74bbc27a648c7e8e861ce15f810de5
Ilya Pridanov <ik.front2558988@gmail.com>
2025-08-05 13:55:47 +0300

    remove:old-files

--- source/js/script.ts
+++ source/js/script.ts
@@ -1,11 +1,4 @@
 import getSlider from './modules/getSlider';
-import sortPopupToggle from './modules/sortPopupToggle';
-import filterPopupToggle from './modules/filterPopupToggle';
-import basketPopupToggle from './modules/basketPopupToggle';
-import getBasket from './modules/getBasket';
 
 getSlider();
-sortPopupToggle();
-filterPopupToggle();
-basketPopupToggle();
-getBasket();
+
